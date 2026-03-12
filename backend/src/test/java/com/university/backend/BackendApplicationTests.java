bcb6b6d19f8d91eecb7bc3902b3d6254cbf7e73b
cosio <cosio@wisc.edu>
2026-03-11 22:45:29 -0500

    Began JUnit Tests Closes #71

--- backend/src/test/java/com/university/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/university/backend/BackendApplicationTests.java
@@ -2,10 +2,13 @@ package com.university.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+//@TestPropertySource(locations="") //placeholder for test config
 class BackendApplicationTests {
 
+	// Default test. Ensures proper loading of reosources.
 	@Test
 	void contextLoads() {
 	}
---
+++ backend/test.sh
@@ -0,0 +1,8 @@
+# placeholder for future containers
+
+# placeholder for docker compose
+
+# tests
+SPRING_profiles_active=dev ./gradlew clean test --no-daemon
+
+# placeholder for if script starts compose
